d3ef8bb6c45ac80fb909f7c7adaf2e6ef86e0faa
risma261 <syafeirisma@students.unnes.ac.id>
2022-04-01 10:42:10 +0700

    add new folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './pages/Home'
+import './assets/styles.css'
+
+function App() {
+  return (
+    <div className="outer-box"> 
+    <Home />
+    </div>
+  );
+}
+
+export default App;
